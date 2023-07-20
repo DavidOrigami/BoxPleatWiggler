f94638fe691fa7e5421f07bb9104fde9e3fa1db0
DavidOrigami <78702389+DavidOrigami@users.noreply.github.com>
2023-07-20 11:43:29 +0200

    Update BoxPleatWiggler.cpp

--- BoxPleatWiggler.cpp
+++ BoxPleatWiggler.cpp
@@ -1,7 +1,3 @@
-// BoxPleatWiggler.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -14,10 +10,9 @@ using std::endl;
 using namespace std;
 
 
-int main()
+int main(int argc, char* argv[])
 {
 	
-	string filename = "";
 	
 	double inputPrecision = 0.00005;
 	bool debugShortenLoop = false;
@@ -34,22 +29,19 @@ int main()
 	
 	Start:
 
-	cout << "Enter filename without '.cp':" << endl;
-	cin >> filename;
+	string filename = argv[1];
+	std::ifstream file(filename);
 
-	fstream file (filename + ".cp");
+	if (!file) {
+		std::cerr << "Error opening file: " << filename << std::endl;
+		return 1;
+	}
+
 	ofstream output(filename + "_fixed.cp");
 
-	if (file.is_open())
-	{
-		cout << "Please enter Grid Size:" << endl;
-		cin >> grid;	
-	}
-	else
-	{
-		cout << "File does not exist" << endl;
-		goto Start;
-	}
+	cout << "Please enter Grid Size:" << endl;
+	cin >> grid;	
+
 
 	double precision = inputPrecision * grid;
 
@@ -143,14 +135,3 @@ int main()
 	while (!_kbhit())
 		continue;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
